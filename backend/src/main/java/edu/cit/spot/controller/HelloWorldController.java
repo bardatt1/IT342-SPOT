bdcb74cc48fa0d1e2c0d04c66e5f40d4d5d194d8
gabejeremy <sandiegogabejeremy@gmail.com>
2025-02-14 14:44:47 +0800

    Initialized Springboot

---
+++ backend/src/main/java/edu/cit/spot/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package edu.cit.spot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
